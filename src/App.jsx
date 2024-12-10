b6fcb014d074b541fd2bcc198ec2dfbf21549174
Kai Ximenes <kaiximenes@outlook.com>
2024-12-10 10:38:09 -0300

    Aula 1 - OK

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+import './App.css'
+import EstilosGlobais from './components/EstilosGlobais'
+
+const FundoGradiente = styled.div`
+  background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+  width: 100%;
+  min-height: 100vh;
+`
+
+function App() {
+
+  return (
+    <FundoGradiente>
+      <EstilosGlobais />
+      
+    </FundoGradiente>
+  )
+}
+
+export default App
